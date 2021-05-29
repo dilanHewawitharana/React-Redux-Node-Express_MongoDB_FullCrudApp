2d0149b578b29371bfef73e74fead5cfda7fccf2
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-05-29 23:34:12 +0900

    update source code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts =  async (req, res) => {
@@ -30,7 +30,7 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatedPost);
 }
