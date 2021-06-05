3e489824b1da2bc97a8ca0fbb1f2252619d7fc38
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-06-05 22:31:49 +0900

    update source code

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended: true}));
@@ -13,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://databaseuser:12341234@cluster0.npy6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://databaseuser:12341234@cluster0.npy6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useFindAndModify: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useFindAndModify: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
