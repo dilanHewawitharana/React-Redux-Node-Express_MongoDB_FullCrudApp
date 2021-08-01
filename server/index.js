b0a2a165a20b1643d56cbcdd6f527ca8ff1d5b3f
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-08-01 23:31:47 +0900

    update source code

--- server/index.js
+++ server/index.js
@@ -6,21 +6,36 @@ import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
+console.log('work here 01');
+
 const app = express();
 dotenv.config();
+
+console.log('work here 02');
 
 app.use(bodyParser.json({ limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended: true}));
 app.use(cors());
 
+console.log('work here 03');
+
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://databaseuser:12341234@cluster0.npy6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+console.log('work here 04');
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
+console.log('work here 05');
+
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useFindAndModify: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+
+console.log('work here 06');
 
 mongoose.set('useFindAndModify', false);
 
