584fcd98a5d5da99b6244043fcc1da4a00368175
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-08-01 23:33:51 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,28 +6,18 @@ import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
-console.log('work here 01');
-
 const app = express();
 dotenv.config();
-
-console.log('work here 02');
 
 app.use(bodyParser.json({ limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended: true}));
 app.use(cors());
 
-console.log('work here 03');
-
 app.use('/posts', postRoutes);
-
-console.log('work here 04');
 
 app.get('/', (req, res) => {
     res.send('Hello to Memories API');
 });
-
-console.log('work here 05');
 
 const PORT = process.env.PORT;
 
