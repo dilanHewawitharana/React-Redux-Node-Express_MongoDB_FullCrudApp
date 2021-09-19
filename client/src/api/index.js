6e3c4b488b6242ad7432d31a89e069b70b70a027
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-09-19 13:55:21 +0900

    finished part 2

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,11 +4,13 @@ const API = axios.create({ baseURL: 'http://localhost:5000'})
 
 // const url = "https://test-memory-app.herokuapp.com/posts";
 
-// API.interceptors.request.use((req) => {
-//     if(localStorage.getItem('profile')) {
-//         req.headers.Authorization
-//     }
-// })
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+})
 
 export const fetchPost = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,6 +11,15 @@ export const getPosts =  async (req, res) => {
     }
 }
 
+export const getPost = async (req, res) => {
+    try {
+        const post = await PostMessage.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 export const createPost = async (req, res) => {
     const post = req.body;
     const newPostMessage = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
@@ -24,13 +33,26 @@ export const createPost = async (req, res) => {
     }
 }
 
+// export const updatePost = async (req, res) => {
+//     const { id} = req.params;
+//     const post = req.body;
+
+//     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+//     const updatedPost = await PostMessage.findByIdAndUpdate(id, { ...post, id }, { new: true });
+
+//     res.json(updatedPost);
+// }
+
 export const updatePost = async (req, res) => {
-    const { id: _id} = req.params;
-    const post = req.body;
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
     res.json(updatedPost);
 }
