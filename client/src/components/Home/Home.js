c91c59d8df99448e735e93bd4d8443ee44d29bc7
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-10-10 19:48:34 +0900

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect} from 'react';
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
+
 import { getPosts } from '../../actions/posts';
 import Pagination from '../Pagination';
 import useStyles from '../../styles';
@@ -8,6 +11,8 @@ import useStyles from '../../styles';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+
+func
 
 const Home = () => {
 
